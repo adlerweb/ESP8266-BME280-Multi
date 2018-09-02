37954eb4872ccf57f0e5541dc8f6580a6a62875a
Florian Knodt <git@adlerweb.info>
2018-09-02 17:59:41 +0200

    Additional computed measurements
    Adds dew point, absolute humidity and absolute atmospheric pressure

--- src/main.cpp
+++ src/main.cpp
@@ -48,20 +48,28 @@ const char* vz_url = "http://192.168.1.1/middleware.php/data/";
 const char* vz_addcmd = ".json?operation=add&value=";
 
 //For UUIDs: Use "" to skip this measurement
-const char* vz_uuid_temp = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee1";
-const char* vz_uuid_hum  = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee2";
-const char* vz_uuid_pres = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee3";
+const char* vz_uuid_temp   = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee1";  //Temperature (°C)
+const char* vz_uuid_dew    = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee2";  //Dew point (°C)
+const char* vz_uuid_hum    = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee3";  //Absolute humidity (g/m3)
+const char* vz_uuid_hum_r  = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee4";  //Relative humidity (%)
+const char* vz_uuid_pres   = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee5";  //Absolute atmospheric pressure (hPa @ sea level)
+const char* vz_uuid_pres_r = "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeee6";  //Relative atmospheric pressure
 
 //periodic status reports
 const unsigned int stats_interval = 60;  // Update statistics and measure every 60 seconds
+
+#define ALTITUDE 123 //Altitude of your location (m above sea level)
 
 Adafruit_BME280 bme; // I2C
 ESP8266WebServer server(80);
 
 #define CONFIG_MQTT_TOPIC_GET "/get"
 #define CONFIG_MQTT_TOPIC_GET_TEMP "/temperature"
-#define CONFIG_MQTT_TOPIC_GET_HUM "/humidity"
+#define CONFIG_MQTT_TOPIC_GET_DEW "/dewpoint"
+#define CONFIG_MQTT_TOPIC_GET_HUM "/humidity_abs"
+#define CONFIG_MQTT_TOPIC_GET_HUMR "/humidity"
 #define CONFIG_MQTT_TOPIC_GET_PRES "/pressure"
+#define CONFIG_MQTT_TOPIC_GET_PRESR "/pressure_rel"
 #define CONFIG_MQTT_TOPIC_SET "/set"
 #define CONFIG_MQTT_TOPIC_SET_RESET "/reset"
 #define CONFIG_MQTT_TOPIC_SET_UPDATE "/update"
@@ -79,6 +87,11 @@ ESP8266WebServer server(80);
 
 #define MQTT_PRJ_HARDWARE "esp8266tls-bme280"
 #define MQTT_PRJ_VERSION "0.0.1"
+
+const float cToKOffset = 273.15;
+float absoluteHumidity(float temperature, float humidity);
+float saturationVaporPressure(float temperature);
+float dewPoint(float temperature, float humidity);
 
 #ifdef USE_MQTT
 class PubSubClientWrapper : public PubSubClient{
@@ -230,27 +243,45 @@ void sendStatsInterval(void) {
   #endif
 
   float temperature = bme.readTemperature();
-  float humidity = bme.readHumidity();
-  float pressure = bme.readPressure() / 100.0F;
+  float humidity_r = bme.readHumidity();
+  float humidity = absoluteHumidity(temperature, humidity_r);
+  float pressure_r = bme.readPressure() / 100.0F;
+  float pressure = bme.seaLevelForAltitude(ALTITUDE, pressure_r);
+  float dew = dewPoint(temperature, humidity_r);
 
   Serial.print(F("T: "));
   Serial.print((String)temperature);
+  Serial.print(F(" *C\nDP: "));
+  Serial.print((String)dew);
   Serial.print(F(" *C\nH: "));
+  Serial.print((String)humidity_r);
+  Serial.print(F(" %\nAH: "));
   Serial.print((String)humidity);
-  Serial.print(F(" %\nP: "));
+  Serial.print(F(" g/m3\nRP: "));
+  Serial.print((String)pressure_r);
+  Serial.print(F(" hPa\nP: "));
   Serial.print((String)pressure);
   Serial.println(F(" hPa"));
+  Serial.flush();
+
+  yield();
 
   #ifdef USE_MQTT
     client.publish(((String)mqtt_root + CONFIG_MQTT_TOPIC_GET + CONFIG_MQTT_TOPIC_GET_TEMP), (String)temperature);
+    client.publish(((String)mqtt_root + CONFIG_MQTT_TOPIC_GET + CONFIG_MQTT_TOPIC_GET_DEW), (String)dew);
     client.publish(((String)mqtt_root + CONFIG_MQTT_TOPIC_GET + CONFIG_MQTT_TOPIC_GET_HUM), (String)humidity);
+    client.publish(((String)mqtt_root + CONFIG_MQTT_TOPIC_GET + CONFIG_MQTT_TOPIC_GET_HUMR), (String)humidity_r);
     client.publish(((String)mqtt_root + CONFIG_MQTT_TOPIC_GET + CONFIG_MQTT_TOPIC_GET_PRES), (String)pressure);
+    client.publish(((String)mqtt_root + CONFIG_MQTT_TOPIC_GET + CONFIG_MQTT_TOPIC_GET_PRESR), (String)pressure_r);
   #endif
 
   #ifdef USE_VOLKSZAEHLER
     sendVz(vz_uuid_temp, temperature);
+    sendVz(vz_uuid_dew, dew);
     sendVz(vz_uuid_hum, humidity);
+    sendVz(vz_uuid_hum_r, humidity_r);
     sendVz(vz_uuid_pres, pressure);
+    sendVz(vz_uuid_pres_r, pressure_r);
   #endif
   
 }
@@ -375,15 +406,21 @@ void setup_wifi() {
 
 void handleRoot() {
   float temperature = bme.readTemperature();
-  float humidity = bme.readHumidity();
-  float pressure = bme.readPressure() / 100.0F;
-
+  float humidity_r = bme.readHumidity();
+  float pressure_r = bme.readPressure() / 100.0F;
+  
   String out = "Temperature: ";
   out += temperature;
-  out += "*C\nHumidity: ";
-  out += humidity;
-  out += "%\nPressure: ";
-  out += pressure;
+  out += "*C\nDew point: ";
+  out += dewPoint(temperature, humidity_r);
+  out += "*C\nRelative Humidity: ";
+  out += humidity_r;
+  out += "%\nAbsolute Humidity: ";
+  out += absoluteHumidity(temperature, humidity_r);
+  out += "g/m3\nRelative Pressure: ";
+  out += pressure_r;
+  out += "hPa\nAbsolute Pressure: ";
+  out += bme.seaLevelForAltitude(ALTITUDE, pressure_r);
   out += "hPa";
   server.send(200, "text/plain", out);
 }
@@ -503,4 +540,85 @@ void loop() {
     sendStatsInterval();
     sendStats=false;
   }
+}
+
+// Relative to absolute humidity
+// Based on https://carnotcycle.wordpress.com/2012/08/04/how-to-convert-relative-humidity-to-absolute-humidity/
+float absoluteHumidity(float temperature, float humidity) {
+  return (13.2471*pow(EULER,17.67*temperature/(temperature+243.5))*humidity/(cToKOffset+temperature));
+}
+
+// Calculate saturation vapor pressure
+// Based on dew.js, Copyright 2011 Wolfgang Kuehn, Apache License 2.0
+float saturationVaporPressure(float temperature) {
+  if(temperature < 173 || temperature > 678) return -112; //Temperature out of range
+
+  float svp = 0;
+  if(temperature <= cToKOffset) {
+    /**
+      * -100-0°C -> Saturation vapor pressure over ice
+      * ITS-90 Formulations by Bob Hardy published in 
+      * "The Proceedings of the Third International 
+      * Symposium on Humidity & Moisture",
+      * Teddington, London, England, April 1998
+      */
+
+    svp = exp(-5.8666426e3/temperature + 2.232870244e1 + (1.39387003e-2 + (-3.4262402e-5 + (2.7040955e-8*temperature)) * temperature) * temperature + 6.7063522e-1 * log(temperature)); 
+  }else{
+    /**
+      * 0°C-400°C -> Saturation vapor pressure over water
+      * IAPWS Industrial Formulation 1997
+      * for the Thermodynamic Properties of Water and Steam
+      * by IAPWS (International Association for the Properties
+      * of Water and Steam), Erlangen, Germany, September 1997.
+      * Equation 30 in Section 8.1 "The Saturation-Pressure 
+      * Equation (Basic Equation)"
+      */
+
+    const float th = temperature + -0.23855557567849 / (temperature - 0.65017534844798e3);
+    const float a  = (th + 0.11670521452767e4) * th + -0.72421316703206e6;
+    const float b  = (-0.17073846940092e2 * th + 0.12020824702470e5) * th + -0.32325550322333e7;
+    const float c  = (0.14915108613530e2 * th + -0.48232657361591e4) * th + 0.40511340542057e6;
+
+    svp = 2 * c / (-b + sqrt(b * b - 4 * a * c));
+    svp *= svp;
+    svp *= svp;
+    svp *= 1e6;
+  }
+  
+  yield();
+
+  return svp;
+}
+
+
+// Calculate dew point in °C
+// Based on dew.js, Copyright 2011 Wolfgang Kuehn, Apache License 2.0
+float dewPoint(float temperature, float humidity)
+{
+  temperature += cToKOffset; //Celsius to Kelvin
+
+  if(humidity < 0 || humidity > 100) return -111; //Invalid humidity
+  if(temperature < 173 || temperature > 678) return -112; //Temperature out of range
+
+  humidity = humidity / 100 * saturationVaporPressure(temperature);
+  
+  byte mc = 10;
+
+  float xNew;
+  float dx;
+  float z;
+
+  do {
+    dx = temperature / 1000;
+    z = saturationVaporPressure(temperature);
+    xNew = temperature + dx * (humidity - z) / (saturationVaporPressure(temperature + dx) - z);
+    if (abs((xNew - temperature) / xNew) < 0.0001) {
+        return xNew - cToKOffset;
+    }
+    temperature = xNew;
+    mc--;
+  } while(mc > 0);
+
+  return -113; //Solver did not get a close result
 }
